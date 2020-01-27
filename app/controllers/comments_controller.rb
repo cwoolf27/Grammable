b1564edc695e22d5b0d8219912f35718a6f6623d
Cactus <cactuswoolf@gmail.com>
2020-01-27 18:40:06 +0000

    add comments test w/ failure

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
+  def create
+    @gram = Gram.find_by_id(params[:gram_id])
+    return render_not_found if @gram.blank?
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:message)
+  end
+end
